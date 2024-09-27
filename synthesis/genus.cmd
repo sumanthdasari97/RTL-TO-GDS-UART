3aac9282c6b4ba2f05fa0c7d48841c5427449dc3
sumanthdasari97 <161044842+sumanthdasari97@users.noreply.github.com>
2024-09-27 16:53:52 -0500

    Create genus.cmd

---
+++ synthesis/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 22.14-s059_1, built Oct 22 2023 08:05:00
+
+# Date: Thu Sep 26 16:48:40 2024
+# Host: kc-sse-tux01.umad.umsystem.edu (x86_64 w/Linux 4.18.0-553.16.1.el8_10.x86_64) (1core*22cpus*43physical cpus*Intel(R) Xeon(R) Gold 6240 CPU @ 2.60GHz 25344KB)
+# OS:   Rocky Linux release 8.10 (Green Obsidian)
+
+source genus_script.tcl
